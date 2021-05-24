51706966fda3419235801d12c18c3e74f68823db
Jared McLaughlin <jared.p.mclaughlin@gmail.com>
2021-05-24 00:30:11 -0400

    Added the ability to do split transactions.

--- src/generateCash/GenerateCash.java
+++ src/generateCash/GenerateCash.java
@@ -17,7 +17,8 @@ public class GenerateCash {
 	static String end;
 	static String account;
 	static StringBuilder accBuild = new StringBuilder(50);
-	static String transfer;
+	static String[] transfer;
+	static Integer[] percents;
 	static StringBuilder tranBuild = new StringBuilder(50);
 	static String filename;
 	
@@ -32,46 +33,65 @@ public class GenerateCash {
 	static boolean linear = false;
 	static boolean constant = false;
 	
+	
+	
 	private static void openFile(String filename) throws IOException {
 		writer = new BufferedWriter(new FileWriter(filename));
 	}
 	
-	private static void writeLine(String date, String account, String description, String transfer, String amount) throws IOException {
-		System.out.println(date + " " + account + " " + description + " " + transfer + " " + amount);
-		
-		writer.write(date + "," + account + "," + description + "," + transfer + ",$" + amount + "\n");
+	private static void writeDeposit(String date, String account, String description, String amount) throws IOException {
+//		System.out.println(date + " " + account + " " + description + " " + amount);		
+		writer.write(date + "," + account + "," + description + ",$" + amount + ",\n");
 	}
 	
+	private static void writeWithdrawl(String date, String account, String description, String amount) throws IOException {
+//		System.out.println(date + " " + account + " " + description + " " + amount);		
+		writer.write(date + "," + account + "," + description + ",,$" + amount + "\n");
+	}
+		
 	private static void closeFile() throws IOException{
 		writer.close();
 	}
 
-	private static void generateEntries(String startDate, String endDate, String account, String transfer) throws IOException{
+	private static void generateEntries(String startDate, String endDate, String account, String[] transfer) throws IOException{
 		Date start = new Date(startDate);
 		Date end = new Date(endDate);
-	
+		Double nAmount;
+		Double sAmount;
+		
 		days = start.daysBetween(end);
 		steps = days / spacing;
 		
 		while(start.lessThan(end)) {
-			writeLine(start.toString(), account, description, transfer, String.format("%.2f",nextAmount()));
+			nAmount = nextAmount();
+			writeDeposit(start.toString(), account, description, String.format("%.2f",nAmount));
+			for(int n = 0; n<transfer.length; n++) {
+				sAmount = nAmount * ((double)(percents[n])/100);
+				writeWithdrawl(start.toString(), transfer[n], description,String.format("%.2f",sAmount));
+			}
 			for(int i = 0; i<spacing; i++) {
 				start.nextDate();
 			}
 		}
 	}
-	
-	private static void generateMonthly(String startDate, String endDate, String account, String transfer) throws IOException{
+
+	private static void generateMonthly(String startDate, String endDate, String account, String[] transfer) throws IOException{
 		Date start = new Date(startDate);
 		Date end = new Date(endDate);
-		
-		
+		Double nAmount;
+		Double sAmount;
 		
 		while(start.lessThan(end)) {
-			writeLine(start.toString(), account, description, transfer, nextAmount().toString());
+			nAmount = nextAmount();
+			writeDeposit(start.toString(), account, description, String.format("%.2f", nAmount));
+			for(int n = 0; n<transfer.length; n++) {
+				sAmount = nAmount * ((double)(percents[n])/100);
+				writeWithdrawl(start.toString(), transfer[n],description, String.format("%.2f", sAmount));
+			}
 			start.nextMonth();
 		}
 	}
+
 	
 	private static Double nextAmount() {
 				
@@ -113,24 +133,43 @@ public class GenerateCash {
 		System.out.println("-s <date>\t(required)\tFirst date of generated entries. ex: -s 11/21/2001");
 		System.out.println("-e <date>\t(required)\tLast date of generated entries. ex: -e 12/31/2001");
 		System.out.println("-i <integer>\t\t\tNumber of days between entries. Defaults to 1. ex: -i 7");
-		System.out.println("-a '<string>'\t(required)\tAccount to create entries for. ex: -a 'Expenses:Insurance'");
-		System.out.println("-t '<string>'\t(required)\tBalancing account for entries. ex: -t 'Assets:Petty Cash'");
+		System.out.println("-a <string>\t(required)\tAccount to create entries for. ex: -a 'Expenses:Insurance'");
+		System.out.println("-t <string>\t(required)\tBalancing account for entries. ex: -t 'Assets:Petty Cash'");
+		System.out.println("\t\tor");
+		System.out.println("-t<integer> <string>\t(required)\tBalancing account for entries. ex: -t50 'Assets:Petty Cash'");		
+		System.out.println("\twhere the integer represents the percentage of the transaction associated with this account");
+		System.out.println("\tin the case that the transcation is split between multiple accounts. This means that there");
+		System.out.println("\tcan be multiple -t entries, but the percentages should add up to 100.");
 		System.out.println("-f <filename>\t(required)\tFilename to output entries to. ex: output.csv");
 		System.out.println("\nOne of the following is required: \n");
-		System.out.println("-d <number>\t(option 1)\tAll entries are the same, and the values is d. ex: -d 50");
+		System.out.println("-d <number>\t(option 1)\tAll entries are the same (constant), and the values is d. ex: -d 50");
 		System.out.println("-r <number>:<number>\t(option 2)\tRandom number between these two. ex: -r 10:100");
 		System.out.println("-r <number>:<number> -m <method>\t(option 3)\tGenerate numbers in the range with the given method.");
-		System.out.println("\tValid methods are linear, random, exponential, s-curve. Random is the same as option 2.");
+//		System.out.println("\tValid methods are linear, random, exponential, s-curve. Random is the same as option 2.");
+		System.out.println("\tValid methods are linear and random. Random is the same as option 2.");
+
 	}
 	
 	public static int parseArgs(String[] args) {
 		int count = args.length;
+		int splits = 0;
 
-		System.out.println("Arg Count" + count);
+		//scan for the amount of transfer accounts, first
+		for(int i = 0; i< count-1; i++) {
+			if(args[i].charAt(0) == '-' && args[i].charAt(1)=='t' && args[i].length() > 2) {
+//				System.out.print(args[i].charAt(2));
+				if(args[i].charAt(2) > 47 && args[i].charAt(2) < 58) splits++;
+//				System.out.print(args[i].charAt(2));
+			}
+		}
 		
-		//if (count % 2 > 0 ) return -1;
-		//else {
-			for(int i = 0; i< count -1; i+=2) {
+		if(splits == 0 ) splits =1;
+		System.out.println("Splits" + splits);
+		percents = new Integer[splits]; // room for split percentages
+		transfer = new String[splits]; // make room for all the split accounts
+		splits = 0;
+		
+		for(int i = 0; i< count -1; i+=2) {
 				//System.out.println(args[i]);
 				if (args[i].charAt(0) == '-') {
 					
@@ -156,14 +195,32 @@ public class GenerateCash {
 							account = accBuild.substring(0, accBuild.length()-1); 
 							break;
 						case 116: // -t transfer account
-							while(i<count-1 && (args[i+1].charAt(0) != '-')) {
-								tranBuild.append(args[i+1]+" ");
-								i++;
+							if(transfer.length == 1) {
+								while(i<count-1 && (args[i+1].charAt(0) != '-')) {
+									tranBuild.append(args[i+1]+" "); // this allows spaces in account names
+									i++;
+								}
+								i--; // don't break for loop
+								//trim the trailing space
+								percents[0] = 100;
+								transfer[0] = tranBuild.substring(0, tranBuild.length()-1); 
+								splits++;
+							}else {
+								// get the percent of the split first
+								percents[splits] = 0;
+								for(int n = 2; n<args[i].length() && args[i].charAt(n) >47 && args[i].charAt(n)<58; n++) {
+									percents[splits] = (percents[splits]*10)+Character.getNumericValue(args[i].charAt(n));
+								}
+								while(i<count-1 && (args[i+1].charAt(0) != '-')){
+									tranBuild.append(args[i+1]+" ");
+									i++;
+								}
+								i--;
+								transfer[splits] = tranBuild.substring(0, tranBuild.length()-1);
+								tranBuild.setLength(0); // clear out for next account
+								splits++;
 							}
 							
-							i--; // don't break for loop
-							//trim the trailing space
-							transfer = tranBuild.substring(0, tranBuild.length()-1); 							
 							break;
 						case 102: // -f file
 							filename = args[i+1];
@@ -256,9 +313,7 @@ public class GenerateCash {
 		}
 		
 		if (arg_res == 2) return;
-		
-		// start, end, increment, account, transfer[percent], filename, amount or baseline + method or least + greatest
-		// -s -e -i -a -t[p] -f -d -b -m -l -g 
+
 		try {
 			openFile(filename);
 			if ( spacing != 30) generateEntries(start, end, account, transfer);
